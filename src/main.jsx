8ff0bda15560b5c275a66bdcf5a1c8047a2c5f22
Rendkrisna19 <muhammadrendykrisna@gmail.com>
2025-10-26 01:36:47 +0700

    fix bug gambar yang tidak muncul di BookController.php

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import Login from "./pages/Login.jsx";
-import UserBooks from "./pages/UserBooks.jsx";
-import AdminLayout from "./pages/admin/AdminLayout.jsx";
-import AdminBooks from "./pages/admin/AdminBooks.jsx";
+import UserBooks from "./pages/UserBukus.jsx";
+import AdminLayout from "./pages/admin/AdminTemplate.jsx";
+import AdminBooks from "./pages/admin/AdminBuku.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
 
 const router = createBrowserRouter([
